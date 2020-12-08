1de65387895765ffbdbd7aaf1e0db6f34d6379a0
Ashikari <12lependu24@gmail.com>
2020-12-08 09:36:14 +0000

    課題10

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>EditProfile</title>
+    </head>
+    <body>
+        <h1>プロフィールを編集する</h1>
+    </body>
+</html>

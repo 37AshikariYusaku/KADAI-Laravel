129efaf7cf1425c8ae722608bed7d03ccdf5d538
Ashikari <12lependu24@gmail.com>
2020-12-10 02:37:42 +0000

    課題11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>私のプロフィール</h1>
-    </body>
-</html>
+<!--<!DOCTYPE html>-->
+<!--<html lang="ja">-->
+<!--    <head>-->
+<!--        <meta charset="utf-8">-->
+<!--        <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--        <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--        <title>MyProfile</title>-->
+<!--    </head>-->
+<!--    <body>-->
+<!--        <h1>私のプロフィール</h1>-->
+<!--    </body>-->
+<!--</html>-->
+
+
+@extends('layouts.profile')
+
+@section('title', 'プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
